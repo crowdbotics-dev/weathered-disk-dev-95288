8a02622cb5252cc2199dbba908dbc52836e38ea8
Crowdbotics <engineering@crowdbotics.com>
2023-06-28 00:47:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-disk-dev-95288",
+  url: "https://weathered-disk-dev-95288.botics.co",
+  api: "https://weathered-disk-dev-95288.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
